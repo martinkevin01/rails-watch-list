7f161c1103fd919bafab66db0e7bca703e3031f1
Martin Kevin Gapie <martinkevingapie@gmail.com>
2023-10-22 22:33:34 -0400

    Delete action bug correction

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  resources :lists, only: %i[index new create show] do
+    resources :bookmarks, only: %i[new create]
+  end
+  resources :bookmarks, only: :destroy
+end
